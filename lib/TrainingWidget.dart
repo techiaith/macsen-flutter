045871a1cf551f292c60880607a96fb45228d33c
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2019-03-14 10:04:29 +0200

    ychwanegu testun esbonio yn screen hyfforddi

--- lib/TrainingWidget.dart
+++ lib/TrainingWidget.dart
@@ -13,36 +13,49 @@ class _TrainingState extends State<TrainingWidget>{
   Widget build(BuildContext context){
     final ApplicationBloc appBloc = ApplicationStateProvider.of(context);
 
-    double mediaWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
-    double width_80 = mediaWidth * 0.8;
-
     appBloc.getUniqueUID().then((uid){
       appBloc.intentParsingBloc.getUnRecordedSentences.add(uid);
     });
 
-    return Center(
-      child:
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    double text_size = 24.0;
+
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          StreamBuilder<String>(
-            stream: appBloc.intentParsingBloc.unRecordedSentenceResult,
-            initialData: '',
-            builder: (context, snapshot) => Container(
-              width: width_80,
-              child: Text(
-                  snapshot.data,
-                  maxLines: 10,
-                  style: TextStyle(fontSize: 24)
+          Container(
+            margin: EdgeInsets.only(top:20.0),
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Text("Mae Macsen yn medru deall eich cwestiynau ddim ond weithiau.", style: TextStyle(fontSize: text_size)),
+          ),
+          Container(
+            margin: EdgeInsets.only(top:10.0),
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Text("Helpwch ni i'w wella drwy recordio rhai ohonynt.", style: TextStyle(fontSize: text_size)),
+          ),
+          Container(
+            margin: EdgeInsets.only(top:10.0),
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: Text("Bydd cwestiynau yn ymddangos isod, pwyswch y botwm meicroffon i ddechrau ac i orffen y recordiad.", style: TextStyle(fontSize: text_size)),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 40.0),
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: StreamBuilder<String>(
+              stream: appBloc.intentParsingBloc.unRecordedSentenceResult,
+              initialData: '',
+              builder: (context, snapshot) => Text(
+                    snapshot.data,
+                    maxLines: 10,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 32.0)
+                ),
               ),
-            ),
           ),
         ],
       ),
     );
   }
-
 }

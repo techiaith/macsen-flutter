4532f2886cc1c1afc1a9d25249bb23de976256e0
Dewi Bryn Jones <d.b.jones@bangor.ac.uk>
2019-03-19 15:13:36 +0200

    testun i sgrin CommonVoice

--- lib/MozillaCommonVoicePage.dart
+++ lib/MozillaCommonVoicePage.dart
@@ -13,17 +13,49 @@ class MozillaCommonVoiceState extends State<MozillaCommonVoicePage> {
 
   @override
   Widget build(BuildContext context){
+
+    double text_size = 22.0;
+
     return Scaffold(
       appBar: AppBar(
         title: new Text("Gwefan Mozilla Common Voice"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: _launchCommonVoiceUrl,
-          child: Text("Agor CommonVoice", style: TextStyle(fontSize: 24.0)),
-        ),
+      body: Container(
+        child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top:20.0),
+                padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Text("Common Voice yw cynllun Mozilla i helpu dysgu peiriannau sut mae pobl go-iawn yn siarad.",
+                    style: TextStyle(fontSize: text_size)),
+              ),
+              Container(
+                margin: EdgeInsets.only(top:20.0),
+                padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Text("I greu systemau lleferydd, mae datblygwyr angen swm sylweddol iawn o recordiadau o leisiau bobl.",
+                    style: TextStyle(fontSize: text_size)),
+              ),
+              Container(
+                margin: EdgeInsets.only(top:20.0),
+                padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Text("Gallwch gyfrannu eich llais i adeiladu cronfa ddata lleisiau fydd pawb yn gallu ei defnyddio i greu rhagor o apiau arloesol ar gyfer dyfeisiau, yr we a'r Gymraeg",
+                    style: TextStyle(fontSize: text_size)),
+              ),
+              Container(
+                margin: EdgeInsets.only(top:20.0),
+                child: RaisedButton(
+                  onPressed: _launchCommonVoiceUrl,
+                  child: Text("Agor CommonVoice",
+                      style: TextStyle(fontSize: 24.0)),
+                ),
+              )
+            ]
+        )
       )
     );
+
   }
 
 
